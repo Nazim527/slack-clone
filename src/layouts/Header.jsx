cf80be41be8569ce1f0f5a1ef2f06e32b00030ac
Nazim Rzayev <nazimrzayev527@gmail.com>
2023-11-27 09:11:33 +0400

    Update File Structure and install Frameworks

---
+++ src/layouts/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header_left">
+            {/* //? Avatars Logged in user */}
+            {/* //? Time Icon */}
+        </div>
+        <div className="header_search">
+            {/* //? Search Icon */}
+            {/* //? Inout */}
+        </div>
+        <div className="header_right">
+            {/* //? Help Icon */}
+        </div>
+    </div>
+  )
+}
+
+export default Header
