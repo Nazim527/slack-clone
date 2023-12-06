f950ce926dba0718c0024f1ec370ba124a1837d0
Nazim Rzayev <nazimrzayev527@gmail.com>
2023-12-07 03:17:22 +0400

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './App.scss'
+import Header from './layouts/header'
+import SideBar from './layouts/SideBar'
 
 function App() {
 
   return (
     <>
       <h1>Slack Clone Lets Go :rocket</h1>
+      <Header/>
+      <div className="app_body">
+        <SideBar/>
+      </div>
     </>
   )
 }
