bf5c213156b8b2c49b7c1e1fb580d6fa8560fd36
Nazim Rzayev <nazimrzayev527@gmail.com>
2023-12-07 03:18:16 +0400

    Header UI Finish

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { Avatar } from '@mui/material'
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import SearchIcon from '@mui/icons-material/Search';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import './header.scss'
 
 const Header = () => {
   return (
@@ -6,13 +11,21 @@ const Header = () => {
         <div className="header_left">
             {/* //? Avatars Logged in user */}
             {/* //? Time Icon */}
+            <Avatar 
+            className='header_avatar'
+            src=''
+            alt='Rzayev Nazim'/>
+            <AccessTimeIcon/>
         </div>
         <div className="header_search">
             {/* //? Search Icon */}
             {/* //? Inout */}
+            <SearchIcon/>
+            <input type="text" placeholder='Search Clever Proggramer'/>
         </div>
         <div className="header_right">
             {/* //? Help Icon */}
+            <HelpOutlineIcon/>
         </div>
     </div>
   )
