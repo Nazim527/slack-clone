dcc7b659a3c8aa0b1426af14dd802e791e53151d
Nazim Rzayev <nazimrzayev527@gmail.com>
2023-12-15 00:45:35 +0400

    FireBase integration Chat

--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -1,29 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
 import { useParams } from "react-router-dom";
 import { InfoOutlined, StarBorderOutlined } from "@mui/icons-material";
+import { collection, doc, onSnapshot, orderBy, query } from "firebase/firestore";
+import room from "../../server";
 
 const ChatBar = () => {
-  const { roomId } = useParams()
-  return (
-  <div className="chatBar">
-    <div className="chat_header">
+  const { roomId } = useParams();
+  const [roomDetails, setRoomDetails] = React.useState(null);
+  const [roomMessage, setRoomMessage] = useState(null)
 
+  React.useEffect(() => {
+    const roomDoc = doc(room, roomId);
+    if (roomId) {
+      onSnapshot(roomDoc, (snapshot) => {
+        setRoomDetails(snapshot.data());
+      });
+    }
+
+    const messagesQuery = query(
+      collection(roomDoc, "messages"),
+      orderBy("timestamp",'asc'),
+    );
+
+    onSnapshot(messagesQuery,snapshoot => {
+      setRoomMessage(snapshoot.docs.map((doc) => doc.data()))
+    })
+
+  }, [roomId]);
+  console.log(roomMessage);
+
+  return (
+    <div className="chatBar">
+      <div className="chat_header">
         <div className="chat_headerLeft">
           <h4 className="chat_channelName">
-            <strong>#general</strong>
-            <StarBorderOutlined/>
+            <strong>#{roomDetails?.name}</strong>
+            <StarBorderOutlined />
           </h4>
         </div>
 
         <div className="chat_headerRight">
           <p>
-            <InfoOutlined/> Details
+            <InfoOutlined /> Details
           </p>
         </div>
+      </div>
     </div>
-  </div>
-  )
+  );
 };
 
 export default ChatBar;
